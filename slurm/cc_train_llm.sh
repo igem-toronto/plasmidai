4105046e4bde98ffe7e6eb66dabdf206a0efb720
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-05-26 14:41:18 -0400

    Update cc_train_llm.sh

--- slurm/cc_train_llm.sh
+++ slurm/cc_train_llm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes 1
-#SBATCH --gres=gpu:4
-#SBATCH --tasks-per-node=4
+#SBATCH --gres=gpu:2
+#SBATCH --tasks-per-node=2
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32GB
-#SBATCH --time=1-00:00
+#SBATCH --time=1-12:00
 #SBATCH --output=logs/%N-%j.out
 
 module load StdEnv/2023 python/3.10 scipy-stack
@@ -23,9 +23,9 @@ export TORCH_NCCL_BLOCKING_WAIT=1
 srun python -m src.experimental.llm.train \
     --accelerator=gpu --devices=4 \
     --precision=bf16-mixed \
-    --batch_size=40 --num_workers=4 \
+    --batch_size=45 --num_workers=4 \
     --enable_fused_add_norm \
     --enable_wandb --wandb_dir="$REPO_ROOT/logs" \
     --enable_checkpoint --checkpoint_dir="$REPO_ROOT/checkpoints/$(date +'%M-%H-%d-%m-%Y')" \
     --enable_progress_bar \
-    --max_epochs=400 --train_steps_per_epoch=500 --val_steps_per_epoch=500 --scheduler_span=50000
+    --max_epochs=125 --train_steps_per_epoch=500 --val_steps_per_epoch=500 --scheduler_span=50000
