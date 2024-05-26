196be7c464c6a8460d4274514f8d02e71d244f12
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-05-26 13:20:07 -0400

    fix params

--- slurm/cc_train_llm.sh
+++ slurm/cc_train_llm.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes 1
 #SBATCH --gres=gpu:4
 #SBATCH --tasks-per-node=4
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=32GB
 #SBATCH --time=1-00:00
 #SBATCH --output=logs/%N-%j.out
@@ -10,23 +10,11 @@
 module load StdEnv/2023 python/3.10 scipy-stack
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
-pip install --no-index torch pytorch_lightning wandb "pydantic<2" einops scipy pandas biopython mamba_ssm
+pip install --no-index "torch<2.3" pytorch_lightning wandb "pydantic<2" einops scipy pandas biopython mamba_ssm
 pip install $HOME/wheels/pydantic_cli-4.3.0-py3-none-any.whl
 
 export REPO_ROOT=~/code/plasmid-ai
 cd $REPO_ROOT
-
-# Copy source code and data
-mkdir -p $SLURM_TMPDIR/plasmid-ai
-cp -r src $SLURM_TMPDIR/plasmid-ai
-cp -r data $SLURM_TMPDIR/plasmid-ai
-cd $SLURM_TMPDIR/plasmid-ai
-
-# Unzip plasmids
-cd data
-gzip -d 240212_plasmid_seq_54646.fasta.gz
-mv 240212_plasmid_seq_54646.fasta plasmids.fasta
-cd ..
 
 wandb offline
 
@@ -34,10 +22,10 @@ export TORCH_NCCL_BLOCKING_WAIT=1
 
 srun python -m src.experimental.llm.train \
     --accelerator=gpu --devices=4 \
-    --precision=16-mixed \
-    --batch_size=16 --num_workers=4 \
+    --precision=bf16-mixed \
+    --batch_size=40 --num_workers=4 \
     --enable_fused_add_norm \
     --enable_wandb --wandb_dir="$REPO_ROOT/logs" \
     --enable_checkpoint --checkpoint_dir="$REPO_ROOT/checkpoints/$(date +'%M-%H-%d-%m-%Y')" \
     --enable_progress_bar \
-    --max_epochs=150 --train_steps_per_epoch=500 --val_steps_per_epoch=500 --scheduler_span=50000
+    --max_epochs=400 --train_steps_per_epoch=500 --val_steps_per_epoch=500 --scheduler_span=50000
