bcae56c00d56d71e5a3eac08ca0b2b91ca1e03bf
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-06-29 19:31:20 -0400

    make logdir

--- src/paths.py
+++ src/paths.py
@@ -5,7 +5,7 @@ CODE_ROOT = pathlib.Path(__file__).parent
 REPO_ROOT = CODE_ROOT.parent
 DATA_ROOT = REPO_ROOT / "data"
 LOG_DIR = REPO_ROOT / "logs"
-
+LOG_DIR.mkdir(exist_ok=True)
 
 def random_checkpoint_dir():
     rand_dir = REPO_ROOT / "checkpoints" / str(uuid.uuid4())
