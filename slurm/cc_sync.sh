01b49239d6deb6de249ca7f7290bcf83d1fd90aa
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-06-29 19:29:15 -0400

    fix path

--- slurm/cc_sync.sh
+++ slurm/cc_sync.sh
@@ -4,4 +4,4 @@ export PROJECT=~/projects/def-mikeuoft/alstonlo
 source $PROJECT/envs/wandb-env/bin/activate
 export REPO_ROOT=$PROJECT/code/plasmid-ai
 cd $REPO_ROOT
-wandb sync logs/wandb/offline-*
+wandb sync $REPO_ROOT/logs/wandb/offline-*
