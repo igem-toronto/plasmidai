9c0656dfae20f68d0abc897bd305cc447999044f
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-06-29 22:34:54 -0400

    Update cc_sync.sh

--- slurm/cc_sync.sh
+++ slurm/cc_sync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+module load StdEnv/2023 python/3.10 scipy-stack
 export PROJECT=~/projects/def-mikeuoft/alstonlo
 source $PROJECT/envs/wandb-env/bin/activate
 export REPO_ROOT=$PROJECT/code/plasmid-ai
