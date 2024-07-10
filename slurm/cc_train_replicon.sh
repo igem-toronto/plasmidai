4ff044dad70a490d387f5518b357cf93348d3898
alstonlo <40709307+alstonlo@users.noreply.github.com>
2024-07-10 19:29:58 -0400

    shrink model size

--- slurm/cc_train_replicon.sh
+++ slurm/cc_train_replicon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes 1
-#SBATCH --gres=gpu:2
-#SBATCH --tasks-per-node=2
-#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=64GB
 #SBATCH --time=1-12:00
 #SBATCH --output=logs/%N-%j.out
@@ -18,10 +18,11 @@ export TORCH_NCCL_BLOCKING_WAIT=1
 
 srun python -m src.experimental.train \
     --backend.matmul_precision=medium \
-    --data RepliconDataModule --data.batch_size=80 --data.num_workers=4 \
-    --lit.fused_add_norm=true --lit.scheduler_span=50000 --lit.num_samples_per_epoch=20 --lit.top_p=0.9 \
-    --trainer.accelerator=gpu  --trainer.devices=2 --trainer.precision=bf16-mixed \
+    --data RepliconDataModule --data.max_tokens=262144 --data.batch_size=160 --data.num_workers=8 \
+    --lit.hidden_features=256 --lit.num_layers=20 \
+    --lit.fused_add_norm=true --lit.scheduler_span=20000 --lit.num_samples_per_epoch=20 --lit.top_p=0.9 \
+    --trainer.accelerator=gpu  --trainer.devices=1 --trainer.precision=bf16-mixed \
     --trainer.wandb=true --trainer.wandb_project=train_replicon_llm --trainer.wandb_dir="${REPO_ROOT}/logs" \
     --trainer.checkpoint=true --trainer.checkpoint_dir="${REPO_ROOT}/checkpoints/$(date +'%Y-%m-%d-%H-%M')" \
     --trainer.progress_bar=true \
-    --trainer.max_epochs=175 --trainer.train_steps_per_epoch=500
+    --trainer.max_epochs=100 --trainer.train_steps_per_epoch=500
