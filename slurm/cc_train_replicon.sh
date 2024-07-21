1c0220f7d3a3ca0d80b439ff02d48cfdd59b8dd2
Adibvafa <adibvafafallahpour@gmail.com>
2024-07-21 15:06:40 -0400

    Fix jsonargparse installation bug.

--- slurm/cc_train_replicon.sh
+++ slurm/cc_train_replicon.sh
@@ -16,7 +16,8 @@ module load StdEnv/2023 python/3.11 scipy-stack
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install -U mamba_ssm=='2.1.0' causal_conv1d=='1.3.0.post1'
-pip install --no-index "torch<2.3" lightning wandb einops scipy pandas biopython transformers jsonargparse
+pip install --no-index "torch<2.3" lightning wandb einops scipy pandas biopython transformers
+pip install $PROJECT/wheels/jsonargparse-4.31.0-py3-none-any.whl
 
 wandb offline
 
