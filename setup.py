d46d5331e1d9f070205e168c7a0af2cfff3821ef
adibvafa <adibvafafallahpour@gmail.com>
2024-09-23 23:47:05 -0400

    Update license.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url="https://github.com/igem-toronto/plasmidai",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
