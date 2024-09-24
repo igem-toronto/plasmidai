37959ca5004191f98cf6ecfe9e2919f7b4fba347
adibvafa <adibvafafallahpour@gmail.com>
2024-09-23 23:45:47 -0400

    Setup Python package.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url="https://github.com/igem-toronto/plasmidai",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache2.0 License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
